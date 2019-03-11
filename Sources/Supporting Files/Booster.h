5cda6c12ad509f5ab84d2a7726471f3f83d97a8c
godpp <taylored@naver.com>
2019-03-12 00:07:02 +0900

    UPDATE : Booster 0.1.0
    - Booster has been rebuilt into Cocoa Touch Framework.
    - Add Travis CI

---
+++ Sources/Supporting Files/Booster.h
@@ -0,0 +1,19 @@
+//
+//  Booster.h
+//  Booster
+//
+//  Created by ParkSungJoon on 11/03/2019.
+//  Copyright © 2019 Park Sung Joon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Booster.
+FOUNDATION_EXPORT double BoosterVersionNumber;
+
+//! Project version string for Booster.
+FOUNDATION_EXPORT const unsigned char BoosterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Booster/PublicHeader.h>
+
+
